055a6de3060de2b6cca288fb3c2dd89fda774c52
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2016-12-17 22:45:21 +0000

    Return (fake) shortened url responses

--- server.js
+++ server.js
@@ -1,21 +1,63 @@
-let express = require('express'),
-    app     = express(),
-    path    = require('path'),
+let express        = require('express'),
+    app            = express(),
+    path           = require('path'),
+    bodyParser     = require('body-parser'),
     testsResponses = require('./tests/test_responses')
 
+function _generateRandomChar() {
+    var characters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    return characters[Math.floor(Math.random() * (characters.length - 1)) + 1]
+}
+
+// Generate unique ID
+function generateUniqueUrl() {
+        var base     = 'http://localhost:3000',
+            uniqueId = '',
+            idLength = 7
+
+    for (var i = 0; i <= idLength; i++) {
+        uniqueId += _generateRandomChar()
+    }
+    return base + '/' + uniqueId
+}
+
+app.use(bodyParser.json())
 app.use('/assets', express.static('assets'))
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'))
 })
 
 app.get('/urls', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(testsResponses.urls));
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(testsResponses.urls))
 })
+
 app.post('/shorten', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(testsResponses.url));
+    var url      = req.body.url,
+        response = JSON.stringify(Object.assign({}, testsResponses.url, {
+            original: url,
+            shortUrl: generateUniqueUrl()
+        }))
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(response)
+})
+
+app.get(/(.*)/, function (req, res) {
+    let requestedUrl = req.params[0].substr(1)
+
+    testsResponses.urls.forEach(function iterateResponses(testResponse) {
+        if(testResponse.shortUrl.match(requestedUrl)) {
+            var redirectTo = testResponse.original
+
+            if(redirectTo.search(/^http[s]?\:\/\//)) {
+                redirectTo = 'http://' + redirectTo
+            }
+            res.redirect(redirectTo)
+        }
+    })
 })
 
 app.listen(3000, function () {
--- tests/test_responses.js
+++ tests/test_responses.js
@@ -1,15 +1,15 @@
 let url = {
     id: 1,
     original: 'www.google.com',
-    shortUrl: 'short.url/d2435g35g',
+    shortUrl: 'http://localhost:3000/d2435g35g',
     created: '1 min ago'
 }
 
 let urls = [
     url,
-    Object.assign({}, url, { id: 2, original: 'www.youtube.com', shortUrl: 'short.url/f344fg4', created: '1 min ago' }),
-    Object.assign({}, url, { id: 3, original: 'www.yahoo.com', shortUrl: 'short.url/93f3dg4', created: '2 mins ago' }),
-    Object.assign({}, url, { id: 4, original: 'www.ask.com', shortUrl: 'short.url/d24f323', created: '6 mins ago' }),
+    Object.assign({}, url, { id: 2, original: 'www.youtube.com', shortUrl: 'http://localhost:3000/f344fg4', created: '1 min ago' }),
+    Object.assign({}, url, { id: 3, original: 'www.yahoo.com', shortUrl: 'http://localhost:3000/93f3dg4', created: '2 mins ago' }),
+    Object.assign({}, url, { id: 4, original: 'www.ask.com', shortUrl: 'http://localhost:3000/d24f323', created: '6 mins ago' }),
 ]
 
 module.exports = {
