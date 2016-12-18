3d34785a391bf4c498cb496d94592456543e20bd
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2016-12-18 00:39:30 +0000

    Attempt at fixing static assets with heroku

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ let express        = require('express'),
     bodyParser     = require('body-parser'),
     testsResponses = require('./tests/test_responses')
 
+process.env.PWD = process.cwd()
+
 function _generateRandomChar() {
     var characters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -23,7 +25,7 @@ function generateUniqueUrl() {
 }
 
 app.use(bodyParser.json())
-app.use('/assets', express.static(__dirname + 'assets'))
+app.use('/assets', express.static(process.env.PWD + 'assets'))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/index.html'))
